e19b11f32336af637b0f2d69234dba5ceeaf74d0
Leonardo Saraiva <leonardosaraiva1911@gmail.com>
2023-01-31 23:30:42 -0300

    Feat: Recebendo dados e validando parâmetros.
    Recebendo dados de um filme e validando os parâmetros recebidos.

---
+++ FilmesApi/FilmesApi/Controllers/FilmeController.cs
@@ -0,0 +1,27 @@
+﻿using FilmesApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController : ControllerBase
+    {
+        private static List<Filme> filmes = new List<Filme>();
+
+        [HttpPost]
+        public void AdicionarFilme([FromBody]Filme filme)
+        {
+            if (!string.IsNullOrEmpty(filme.Titulo) &&
+                filme.Titulo.Length <= 500 &&
+                !string.IsNullOrEmpty(filme.Genero) &&
+                filme.Duracao >= 70)
+            {
+                filmes.Add(filme);
+                Console.WriteLine(filme.Titulo);
+                Console.WriteLine(filme.Genero);
+                Console.WriteLine(filme.Duracao);
+            }
+        }
+    }
+}
